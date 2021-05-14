d0e02e7a2379232969eeb10640dfc153508fbf71
suunkl12 <40013740+suunkl12@users.noreply.github.com>
2021-05-14 08:23:18 +0700

    update gun

---
+++ Assets/Scripts/JavaServer/Game_ManagerJava.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Game_ManagerJava : MonoBehaviour
+{
+    //public GameObject GunContainer;
+
+    //public Gun[] slotGun;
+
+    //public Gun playerIn;
+
+    //public GameEvent gameEvent;
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    gameEvent.SetGunNotInUsed += TakeGunBack;
+    //    gameEvent.OnConnect += SetRandom;
+    //    slotGun = new Gun[GunContainer.gameObject.transform.childCount];
+    //    for (int i = 0; i < GunContainer.gameObject.transform.childCount; i++)
+    //    {
+    //        slotGun[i] = GunContainer.gameObject.transform.GetChild(i).gameObject.GetComponent<Gun>();
+    //    }
+    //}
+
+    //void setSlot()
+    //{
+    //    for (int i = 0; i < slotGun.Length; i++)
+    //    {
+    //        if (!slotGun[i].inUsed)
+    //        {
+    //            slotGun[i].inUsed = true;
+    //            playerIn = slotGun[i];
+                
+    //            //Debug.Log("You are in " + slotGun[i]);
+    //            break;
+    //        }
+    //        else
+    //            continue;
+    //    }
+    //}
+
+
+    //void SetRandom()
+    //{
+    //    int i;
+    //    do
+    //    {
+    //        //TODO set gun to random
+    //        i = Random.Range(0, slotGun.Length - 1);
+    //        if (!slotGun[i].inUsed)
+    //        {
+    //            slotGun[i].SetGunInUsed();
+    //            TargetSetPlayerIn(target, i);
+    //            //Debug.Log("You are in " + slotGun[i]);
+    //            break;
+    //        }
+            
+    //    } while (true);
+    //}
+
+    ////Set tình trạng sử dụng của trúng là không có ai xài trên server
+    //public void TakeGunBack(int index)
+    //{
+    //    Debug.Log("Set gun " + index + " not in used");
+    //    slotGun[index].ResetGun();
+    //}
+
+    ////Set súng này đang được sử dụng bởi người chơi trên client
+    //public void TargetSetPlayerIn(NetworkConnection connection, int i)
+    //{
+    //    playerIn = slotGun[i];
+    //}
+    
+}
--- Assets/Scripts/JavaServer/NetworkManager.cs
+++ Assets/Scripts/JavaServer/NetworkManager.cs
@@ -1,18 +1,43 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System;
+using System.Linq;
 using System.Collections.Generic;
-using UnityEngine;
-
+using System.Collections;
+using System.IO;
 public class NetworkManager : MonoBehaviour
 {
+    public static string ip = "127.0.0.1";
+    public static short port = 26000;
+    public static NetworkManager instance;
+    private static TcpClient tcp;
+
+    public bool connected = false;
+    public Thread threadReceive;
+    public Thread threadAttempting;
+
+    public IPEndPoint IpTCP;
     // Start is called before the first frame update
     void Start()
     {
-        
+        instance = this;
+        IPAddress mIp = IPAddress.Parse(ip);
+        IpTCP = new IPEndPoint(mIp, 26000);
+        instance = this;
+        tcp = new TcpClient();
+        tcp.NoDelay = true;
+        Debug.Log("Connecting...");
+        tcp.Connect(IpTCP);
+        Debug.Log("Connected");
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+
+    
+
+    
+
 }
